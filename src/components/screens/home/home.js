4ce9c00501ddf3d5e84be82f81bebfd60f4f01be
WaqasMumtaz <waqasali.mumtaz95@gmail.com>
2020-10-14 16:29:11 +0500

    add input value useState

--- src/components/screens/home/home.js
+++ src/components/screens/home/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 // import history from '../../History'; 
 import { Link } from 'react-router-dom';
 // import { Provider } from '../../Context'; 
@@ -6,13 +6,26 @@ const data={
     a:'1', b:'2'
 }
 const Home=()=>{
+  const [palin , setPalin] = useState('');
+
+  useEffect(()=>{
+    console.log('Palin length >>', palin.length);
+    
+  },[palin])
+
 
   return (
     // <Provider>
     <div className="App">
       <h1>Wellcome To The Home Page</h1>
       <br></br>
-      {/*<Link to="/products">Click here</Link>*/}
+      <form>
+  <label >
+    Value:
+    <input type="text" value={palin} onChange={e => setPalin(e.target.value)}/>
+  </label>
+  <input type="submit" value="Submit" />
+</form>
      <Link to={'/products'}> 
        <button>Go To Products</button>
      </Link>
